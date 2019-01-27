30166d8f1263dc6590a23396ad69d02d17bd9c62
William Martin <wmartin@pivotal.io>
2019-01-27 14:20:20 +0100

    Add validation tags for microservice info

--- info.go
+++ info.go
@@ -2,24 +2,24 @@ package omg
 
 type Info struct {
 	// Version specifies version of the microservice.
-	Version string `json:"version,omitempty"`
+	Version string `json:"version,omitempty" jsonschema:"required,pattern=[0-9]*\\.[0-9]*\\.[0-9]*"`
 
 	// Title specifies a human friendly name.
-	Title string `json:"title,omitempty"`
+	Title string `json:"title,omitempty" jsonschema:"required"`
 
 	// Description specifies a human friendly description.
-	Description string `json:"description,omitempty"`
+	Description string `json:"description,omitempty" jsonschema:"required"`
 
 	// Contact specifies a contact point.
 	Contact *Contact `json:"contact,omitempty"`
 
 	// License describes the licensing model.
-	License *License `json:"license,omitempty"`
+	License *License `json:"license,omitempty" jsonschema:"required"`
 }
 
 type Contact struct {
 	// Name is the name of the person or company.
-	Name string `json:"name,omitempty"`
+	Name string `json:"name,omitempty" jsonschema:"required"`
 
 	// URL is the homepage of the person or company.
 	URL string `json:"url,omitempty"`
@@ -30,8 +30,8 @@ type Contact struct {
 
 type License struct {
 	// License is the name of the license.
-	Name string `json:"name,omitempty"`
+	Name string `json:"name,omitempty" jsonschema:"required"`
 
 	// URL is a url to an indepth explanation of the license.
-	URL string `json:"url,omitempty"`
+	URL string `json:"url,omitempty" jsonschema:"required"`
 }
--- validation_test.go
+++ validation_test.go
@@ -31,6 +31,66 @@ var _ = Describe("Schema Validation", func() {
 			})
 		})
 	})
+
+	Describe("Info", func() {
+		Describe("the microservice version", func() {
+			It("is required", func() {
+				microservice := &Microservice{Info: &Info{}}
+				valid, errors := validate(microservice)
+				Expect(valid).To(BeFalse())
+				Expect(errors).To(ContainElement("info: version is required"))
+			})
+
+			It("must be a semantic version", func() {
+				microservice := &Microservice{Info: &Info{Version: "not-valid"}}
+				valid, errors := validate(microservice)
+				Expect(valid).To(BeFalse())
+				Expect(errors).To(ContainElement("info.version: Does not match pattern '[0-9]*\\.[0-9]*\\.[0-9]*'"))
+			})
+		})
+
+		Describe("the title", func() {
+			It("is required", func() {
+				microservice := &Microservice{Info: &Info{}}
+				valid, errors := validate(microservice)
+				Expect(valid).To(BeFalse())
+				Expect(errors).To(ContainElement("info: title is required"))
+			})
+		})
+
+		Describe("the description", func() {
+			It("is required", func() {
+				microservice := &Microservice{Info: &Info{}}
+				valid, errors := validate(microservice)
+				Expect(valid).To(BeFalse())
+				Expect(errors).To(ContainElement("info: description is required"))
+			})
+		})
+
+		Describe("the license", func() {
+			It("is required", func() {
+				microservice := &Microservice{Info: &Info{}}
+				valid, errors := validate(microservice)
+				Expect(valid).To(BeFalse())
+				Expect(errors).To(ContainElement("info: license is required"))
+			})
+
+			It("requires a name", func() {
+				microservice := &Microservice{Info: &Info{License: &License{}}}
+				valid, errors := validate(microservice)
+				Expect(valid).To(BeFalse())
+				Expect(errors).To(ContainElement("info.license: name is required"))
+			})
+
+			It("requires a url", func() {
+				microservice := &Microservice{Info: &Info{License: &License{}}}
+				valid, errors := validate(microservice)
+				Expect(valid).To(BeFalse())
+				Expect(errors).To(ContainElement("info.license: url is required"))
+			})
+		})
+	})
+
 })
 
 func validate(microservice *Microservice) (bool, []string) {
